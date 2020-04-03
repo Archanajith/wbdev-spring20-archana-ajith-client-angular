933c1ae21a1c38156dc6551e79e6b352778cbeb3
Archanajith <46067531+Archanajith@users.noreply.github.com>
2020-04-03 13:24:46 -0400

    Changed to https

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -3,9 +3,9 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/archanajith/courses')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/archanajith/courses')
       .then(response => response.json())
   findCourseById = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/archanajith/courses/${courseId}`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/archanajith/courses/${courseId}`)
       .then(response => response.json())
 }
